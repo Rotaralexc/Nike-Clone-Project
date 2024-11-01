e57fb23ecedce171501daf1372b4ab9dcc9404d9
RotarAlexC <rotaralexc@gmail.com>
2024-11-01 20:41:39 +0200

    Create Nike Project

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+    Services,
+    Footer,
+    CustomerReviews
+}
